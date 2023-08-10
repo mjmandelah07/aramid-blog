f82c5db4ae5fda6adfe9036f0c3d103bc2d98754
mjmandelah07 <aramidemojisolagrace@gmail.com>
2023-08-10 02:09:49 +0100

    added backend url

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ const connectDB = require("./database/db");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 
-
 //router routes
 const categoriesRouter = require("./routes/categories");
 const authRouter = require("./routes/auth");
@@ -13,11 +12,13 @@ const commentsRouter = require("./routes/comments");
 const adminProfile = require("./routes/adminProfile");
 const clickCountRouter = require("./routes/clickCount");
 
-
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
+const corsOptions = {
+  origin: "https://aramid-client-blog.onrender.com/",
+};
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(bodyParser.json());
@@ -26,15 +27,14 @@ app.use(bodyParser.json());
 connectDB();
 
 // endpoint for login page
-app.use('/api/auth', authRouter)
+app.use("/api/auth", authRouter);
 
-
- // Import the new route
+// Import the new route
 app.use("/api/click-count", clickCountRouter);
-app.use('/api/categories', categoriesRouter);
-app.use('/api/articles', articlesRouter);
-app.use('/api/comments', commentsRouter);
-app.use('/api/admin', adminProfile);
+app.use("/api/categories", categoriesRouter);
+app.use("/api/articles", articlesRouter);
+app.use("/api/comments", commentsRouter);
+app.use("/api/admin", adminProfile);
 
 // Start the server
 app.listen(port, () => {
